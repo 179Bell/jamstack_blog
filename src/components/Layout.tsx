50b9e33e0e97aa5c187c9d65022bdb96b0cebd80
179Bell <shingo.msdf@gmail.com>
2023-11-07 21:06:45 +0900

    ブログカラーとフォントを設定

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,20 @@
+// eslint-disable-next-line camelcase
+import { Noto_Sans_JP } from 'next/font/google';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+
+const notoJp = Noto_Sans_JP({
+  weight: ['400', '500', '700'],
+  display: 'swap',
+  variable: '--font-notojp',
+  preload: false,
+});
 
 export default function Layout({ children }: any) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main className={`${notoJp.variable} font-notojp`}>{children}</main>
       <Footer />
     </>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,14 +7,17 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      theme: '#FAF8F2',
+      sub: '#7BB6E3',
+      accents: '#007bbb',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        notojp: ['var(--font-notojp)'],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
