788cf2030bee41b4edd42fefc29f9b2b6a3c5ceb
179Bell <shingo.msdf@gmail.com>
2023-11-11 21:09:43 +0900

    feat:コードブロックのスタイリング追加

--- src/components/MarkdownTemplate.tsx
+++ src/components/MarkdownTemplate.tsx
@@ -1,9 +1,27 @@
-import parse from 'html-react-parser';
+import highlight, { AutoHighlightResult } from 'highlight.js';
+import 'highlight.js/styles/hybrid.css';
+import parse, { HTMLReactParserOptions } from 'html-react-parser';
 
 type Props = {
   props: string;
 };
 
+const options: HTMLReactParserOptions = {
+  replace: (domNode: any) => {
+    if (domNode.name === 'code' && domNode.parent.name === 'pre') {
+      const code: string = domNode.children[0].data;
+      const highlightedCode: AutoHighlightResult =
+        highlight.highlightAuto(code);
+
+      return (
+        <pre>
+          <code className="hljs">{parse(highlightedCode.value)}</code>
+        </pre>
+      );
+    }
+  },
+};
+
 export const MarkdownTemplate = ({ props }: Props) => {
-  return <>{parse(props)}</>;
+  return <>{parse(props, options)}</>;
 };
