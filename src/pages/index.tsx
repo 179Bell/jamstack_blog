57592e1394b70e6d447b4686702d133cae1242df
179Bell <shingo.msdf@gmail.com>
2023-11-14 22:00:26 +0900

    feat:PostCardコンポーネントのスタイル修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home({ blogs }: Props) {
     <>
       <div className="flex flex-row justify-between container h-full md:mx-auto p-4">
         <div className="w-full md:w-8/12">
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-5">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-5">
             {blogs.map((blog) => (
               <PostCard key={blog.id} blog={blog} />
             ))}
