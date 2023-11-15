6ecf790ab4a340e85d77a7c8a0accfb1c0d1cee5
179Bell <shingo.msdf@gmail.com>
2023-11-16 07:05:12 +0900

    Layout.tsxを2カラムになるように修正

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,7 +14,11 @@ export default function Layout({ children }: any) {
   return (
     <>
       <Header />
-      <main className={`${notoJp.variable}`}>{children}</main>
+      <main className={`${notoJp.variable}`}>
+        <div className="flex flex-row justify-between container h-full md:mx-auto p-4">
+          <div className="w-full md:w-8/12">{children}</div>
+        </div>
+      </main>
       <Footer />
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,14 +9,10 @@ type Props = {
 export default function Home({ blogs }: Props) {
   return (
     <>
-      <div className="flex flex-row justify-between container h-full md:mx-auto p-4">
-        <div className="w-full md:w-8/12">
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-5">
-            {blogs.map((blog) => (
-              <PostCard key={blog.id} blog={blog} />
-            ))}
-          </div>
-        </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-5">
+        {blogs.map((blog) => (
+          <PostCard key={blog.id} blog={blog} />
+        ))}
       </div>
     </>
   );
