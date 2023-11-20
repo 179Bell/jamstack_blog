e651e1e732638297f6bb0e79a59ea793eec5c3dd
179Bell <shingo.msdf@gmail.com>
2023-11-20 19:51:04 +0900

    feat/titleをブログ名に変更

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Sidebar({ children }: any) {
-  return <div className="w-1/4">{children}</div>;
+  return <div className="hidden md:block md:w-1/4">{children}</div>;
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ja" className="font-notojp">
-      <Head />
+      <Head>
+        <title>すずむらの積み上げblog</title>
+      </Head>
       <body className="bg-theme">
         <Main />
         <NextScript />
