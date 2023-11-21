75666681edbbf0661202f83b6bc66442fc13402b
179Bell <shingo.msdf@gmail.com>
2023-11-21 21:46:47 +0900

    APIのリトライオプションを追加

--- libs/client.ts
+++ libs/client.ts
@@ -3,4 +3,5 @@ import { createClient } from 'microcms-js-sdk';
 export const client = createClient({
   serviceDomain: process.env.SERVICE_DOMAIN || '',
   apiKey: process.env.API_KEY || '',
+  retry: true,
 });
